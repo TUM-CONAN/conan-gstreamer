ed27915d9e76c4678bbdcebe61a8a2cb4dc62dab
Ulrich Eck <ulrich.eck@tum.de>
2020-04-26 10:12:42 +0200

    compiles linux camp conan

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@ from conans import ConanFile, Meson, tools
 
 class GStreamerConan(ConanFile):
     name = "gstreamer"
+    version = "1.16.2"
     description = "A framework for streaming media"
     license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
@@ -18,33 +19,39 @@ class GStreamerConan(ConanFile):
         "tools=True",
     )
 
-    def set_version(self):
-        git = tools.Git(folder=self.recipe_folder)
-        tag, branch = git.get_tag(), git.get_branch()
-        self.version = tag if tag and branch.startswith("HEAD") else branch
+    generators = "pkgconf"
+
+    # def set_version(self):
+    #     git = tools.Git(folder=self.recipe_folder)
+    #     tag, branch = git.get_tag(), git.get_branch()
+    #     self.version = tag if tag and branch.startswith("HEAD") else branch
 
     def build_requirements(self):
-        self.build_requires("generators/1.0.0@%s/stable" % self.user)
-        self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
-        self.build_requires("bison/[>=3.3]@%s/stable" % self.user)
-        self.build_requires("flex/[>=2.6.4]@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@camposs/stable")
+        self.build_requires("meson/[>=0.51.2]@camposs/stable")
+        self.build_requires("pkgconf/1.6.3@camposs/stable")
         if self.options.introspection:
-            self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user, )
+            self.build_requires("gobject-introspection/[>=1.59.3]@camposs/stable")
 
     def requirements(self):
-        self.requires("glib/[>=2.62.0]@%s/stable" % self.user)
+        self.requires("glib/[>=2.62.0]@camposs/stable")
 
     def source(self):
         git = tools.Git(folder="%s-%s" % (self.name, self.version))
         git.clone(url="https://gitlab.freedesktop.org/gstreamer/gstreamer.git", branch=self.version, shallow=True)
 
     def build(self):
+        pkg_config_paths = []
+        if "PKG_CONFIG_PATH" in os.environ:
+            pkg_config_paths.extend(os.environ["PKG_CONFIG_PATH"].split(":"))
+        pkg_config_paths.append(self.build_folder)
+
         args = ["--auto-features=disabled"]
         args.append("-Dintrospection=" + ("enabled" if self.options.introspection else "disabled"))
         args.append("-Dcheck=" + ("enabled" if self.options.check else "disabled"))
         args.append("-Dtools=" + ("enabled" if self.options.tools else "disabled"))
         meson = Meson(self)
-        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args, pkg_config_paths=pkg_config_paths)
         meson.install()
 
     def package_info(self):
